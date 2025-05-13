d8190e5d0d823398d22291b900bb7d161cd3a64c
Camilo Barreto <camilo.barreto@sinco.co>
2025-05-12 21:24:04 -0500

    feat(ejercicio2: version antes de la revision

--- Workshops/IntroductionToEventSourcing/02-GettingStateFromEvents/Immutable/GettingStateFromEventsTests.cs
+++ Workshops/IntroductionToEventSourcing/02-GettingStateFromEvents/Immutable/GettingStateFromEventsTests.cs
@@ -2,6 +2,7 @@ using FluentAssertions;
 using Xunit;
 
 namespace IntroductionToEventSourcing.GettingStateFromEvents.Immutable;
+
 using static ShoppingCartEvent;
 
 // EVENTS
@@ -33,7 +34,7 @@ public abstract record ShoppingCartEvent
     ): ShoppingCartEvent;
 
     // This won't allow external inheritance
-    private ShoppingCartEvent(){}
+    private ShoppingCartEvent() { }
 }
 
 // VALUE OBJECTS
@@ -51,7 +52,80 @@ public record ShoppingCart(
     PricedProductItem[] ProductItems,
     DateTime? ConfirmedAt = null,
     DateTime? CanceledAt = null
-);
+)
+{
+    public static ShoppingCart Evolve(ShoppingCartEvent @event, ShoppingCart shoppingCart)
+    {
+        return @event switch
+        {
+            ShoppingCartOpened opened => Open(opened),
+            ProductItemAddedToShoppingCart productItemAdded => AddProductItem(productItemAdded, shoppingCart),
+            ProductItemRemovedFromShoppingCart productItemRemoved =>
+                RemoveProductItem(productItemRemoved, shoppingCart),
+            ShoppingCartConfirmed confirmed => Confirm(confirmed, shoppingCart),
+            ShoppingCartCanceled canceled => Canceled(canceled, shoppingCart),
+            _ => shoppingCart
+        };
+    }
+
+    private static ShoppingCart Canceled(ShoppingCartCanceled canceled, ShoppingCart shoppingCart)
+    {
+        return shoppingCart with { CanceledAt = canceled.CanceledAt, Status = ShoppingCartStatus.Canceled };
+    }
+
+    private static ShoppingCart Confirm(ShoppingCartConfirmed confirmed, ShoppingCart shoppingCart)
+    {
+        return shoppingCart with { Status = ShoppingCartStatus.Confirmed, ConfirmedAt = confirmed.ConfirmedAt };
+    }
+
+    private static ShoppingCart RemoveProductItem(ProductItemRemovedFromShoppingCart productItemRemoved,
+        ShoppingCart shoppingCart)
+    {
+        var currentProductItems = shoppingCart.ProductItems.ToList();
+        var product = currentProductItems.Single(p => p.ProductId == productItemRemoved.ProductItem.ProductId);
+        var index = currentProductItems.FindIndex(p => p.ProductId == product.ProductId);
+
+        var quantity = product.Quantity - productItemRemoved.ProductItem.Quantity;
+
+
+        if (quantity > 0)
+            currentProductItems[index] = product with { Quantity = quantity };
+        else
+            currentProductItems.Remove(product);
+
+        return shoppingCart with { ProductItems = currentProductItems.ToArray() };
+    }
+
+    private static ShoppingCart Open(ShoppingCartOpened opened)
+    {
+        return new ShoppingCart(opened.ShoppingCartId,
+            opened.ClientId,
+            ShoppingCartStatus.Pending,
+            []);
+    }
+
+    private static ShoppingCart AddProductItem(ProductItemAddedToShoppingCart productItemAdded,
+        ShoppingCart shoppingCart)
+    {
+        var currentProductItems = shoppingCart.ProductItems.ToList();
+        var product = currentProductItems.SingleOrDefault(p => p.ProductId == productItemAdded.ProductItem.ProductId);
+
+        if (product == null)
+            currentProductItems.Add(productItemAdded.ProductItem);
+        else
+        {
+            currentProductItems.Remove(product);
+            currentProductItems.Add(product with { Quantity = product.Quantity + productItemAdded.ProductItem.Quantity });
+        }
+
+        return shoppingCart with { ProductItems = currentProductItems.ToArray() };
+    }
+
+    public static ShoppingCart Empty()
+    {
+        return new ShoppingCart(Guid.Empty, Guid.Empty, ShoppingCartStatus.Pending, []);
+    }
+};
 
 public enum ShoppingCartStatus
 {
@@ -63,8 +137,18 @@ public enum ShoppingCartStatus
 public class GettingStateFromEventsTests
 {
     // 1. Add logic here
-    private static ShoppingCart GetShoppingCart(IEnumerable<ShoppingCartEvent> events) =>
-        throw new NotImplementedException();
+    private static ShoppingCart GetShoppingCart(IEnumerable<ShoppingCartEvent> events)
+    {
+        var shoppingCart = ShoppingCart.Empty();
+
+        foreach (var @event in events)
+        {
+            shoppingCart = ShoppingCart.Evolve(@event, shoppingCart);
+        }
+
+        return shoppingCart;
+    }
+
 
     [Fact]
     [Trait("Category", "SkipCI")]
